c3e122584b592a55c3b5ae05defc7fe3e9ebaca7
AbdulRahman01200 <141459722+AbdulRahman01200@users.noreply.github.com>
2023-08-07 16:42:36 +0300

    Add files via upload

---
+++ Style/bootstrap-5.3.1-dist/js/LAPMELOM.js
@@ -0,0 +1,70 @@
+function LOGO() {
+    document.getElementById('LAPMELON').style.width = '36px';
+    document.getElementById('LAPMELON').style.height = '29px';
+    document.getElementById('LAPMELON').style.transition = '0.5s';
+}
+function LOGOUP() {
+    document.getElementById('LAPMELON').style.width = '30px';
+    document.getElementById('LAPMELON').style.height = '24px';
+    document.getElementById('LAPMELON').style.transition = '0.5s';
+}
+function infolist() {
+    document.getElementById('infolist').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist').style.transition = '0.5s';
+    document.getElementById('infolist').style.letterSpacing = '0.5px'
+}
+function infolistout() {
+    document.getElementById('infolist').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist').style.letterSpacing = '0px'
+    document.getElementById('infolist').style.transition = '0.5s';
+}
+function infolist2() {
+    document.getElementById('infolist2').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist2').style.letterSpacing = '0.5px';
+    document.getElementById('infolist2').style.transition = '0.5s';
+}
+function infolistout2() {
+    document.getElementById('infolist2').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist2').style.letterSpacing = '0px'
+    document.getElementById('infolist2').style.transition = '0.5s';
+}
+function infolist3() {
+    document.getElementById('infolist3').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist3').style.letterSpacing = '0.5px'
+    document.getElementById('infolist3').style.transition = '0.5s';
+}
+function infolistout3() {
+    document.getElementById('infolist3').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist3').style.letterSpacing = '0px'
+    document.getElementById('infolist3').style.transition = '0.5s';
+}
+function infolist4() {
+    document.getElementById('infolist4').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist4').style.letterSpacing = '0.5px'
+    document.getElementById('infolist4').style.transition = '0.5s';
+}
+function infolistout4() {
+    document.getElementById('infolist4').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist4').style.letterSpacing = '0px'
+    document.getElementById('infolist4').style.transition = '0.5s';
+}
+function infolist5() {
+    document.getElementById('infolist5').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist5').style.letterSpacing = '0.5px'
+    document.getElementById('infolist5').style.transition = '0.5s';
+}
+function infolistout5() {
+    document.getElementById('infolist5').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist5').style.letterSpacing = '0px'
+    document.getElementById('infolist5').style.transition = '0.5s';
+}
+function infolist6() {
+    document.getElementById('infolist6').style.backgroundColor = 'palevioletred'
+    document.getElementById('infolist6').style.letterSpacing = '0.5px'
+    document.getElementById('infolist6').style.transition = '0.5s';
+}
+function infolistout6() {
+    document.getElementById('infolist6').style.backgroundColor = 'rgb(223, 163, 183)'
+    document.getElementById('infolist6').style.letterSpacing = '0px'
+    document.getElementById('infolist6').style.transition = '0.5s';
+}
